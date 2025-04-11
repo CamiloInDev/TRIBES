db0f99ab858e54a02ade768c79b3c535a1d933e7
CamiloInfanteG <camilonsc666@gmail.com>
2025-04-10 20:17:14 -0500

    avances
    aaaa

--- tiendapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# app_name = 'tiendapp'  # Opcional: Esto crea un namespace para tus URLs
-
-urlpatterns = [
-    path('', views.home, name='Home'),
-    path('colecciones/', views.colecciones, name='colecciones'),
-]
--- tiendapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request, 'tiendapp/home.html')  
-
-def colecciones(request):
-    return render(request, 'tiendapp/colecciones.html')
